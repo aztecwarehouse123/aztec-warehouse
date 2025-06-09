f858fc71ab40d8e46ab7c72d999dcfa6cc340d6d
aztecwarehouse123 <aztec.warehouse.mgt@gmail.com>
2025-06-09 16:10:28 +0500

    adsad

--- src/main.tsx
+++ src/main.tsx
@@ -9,7 +9,7 @@ import { ThemeProvider } from './contexts/ThemeContext';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <Router>
+    <Router basename='/aztec-warehouse'>
       <ThemeProvider>
       <AuthProvider>
         <ToastProvider>
