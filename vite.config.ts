0d51a9877d59bbf8bbb465217a7b36c313f193dc
aztecwarehouse123 <aztec.warehouse.mgt@gmail.com>
2025-06-23 23:34:38 +0500

    add CNAMEs

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,8 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/aztec-warehouse/',
+  //base: '/aztec-warehouse/',
+  base: '/',
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
